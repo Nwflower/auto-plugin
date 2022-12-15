9983b0b2f196c0e7a50fb4e30a70e2de6f031a5b
Nwflower <1679659@qq.com>
2022-12-15 13:44:00 +0800

    对于手动at过多成员的消息，依然生效

--- app/autoDelAt.js
+++ app/autoDelAt.js
@@ -17,13 +17,38 @@ export class autoDelAt extends plugin {
     Object.defineProperty(rule, 'log', { get: () => this.islog })
   }
 
+  async getGroupSize () {
+    let MemberMap = await this.e.group.getMemberMap()
+    let allmember = []
+    MemberMap.forEach((v, k) => { allmember.push(k) })
+    logger.info(allmember.length)
+    return allmember.length
+  }
+
+  async kick () {
+    if(this.e.member.is_admin && this.e.member.is_owner) return false
+    this.islog = true
+    await this.reply(`发现QQ${this.e.user_id}恶意at，已经将其移出群`)
+    await this.e.group.kickMember(this.e.user_id)
+  }
+
   async atListen (e) {
     if (!Bot.pickGroup(this.e.group_id).is_owner && !Bot.pickGroup(this.e.group_id).is_admin) {
       // do nth
-    } else if (e.atall && !e.member.is_admin && !e.member.is_owner ) {
-      await this.e.group.kickMember(this.e.user_id)
-      await this.reply(`QQ${this.e.user_id}恶意at全体成员，已经将其移出群`)
-      this.islog = true
+    } else {
+      let AtQQ = []
+      for (let msg of e.message) {
+        if (msg.type === 'at') {
+          AtQQ.push(msg.qq)
+        }
+      }
+      if (!AtQQ.length) {
+        return false
+      } else if (AtQQ.length>=10 && AtQQ.length>=Number(await this.getGroupSize() * 0.8)) {
+        await this.kick()
+      } else if (e.atall) {
+        await this.kick()
+      }
     }
     return this.islog
   }
