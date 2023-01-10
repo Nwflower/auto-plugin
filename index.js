5f0b70986b9939ea51031e29dbaa1fff3623a5f7
Nwflower <1679659@qq.com>
2023-01-10 21:35:19 +0800

    群名片自动复位

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs'
+import { currentVersion } from "../flower-plugin/components/Changelog.js";
 
 logger.info('---------!_!---------')
 logger.info(`自动化插件1.0.1载入成功`)
@@ -11,6 +12,15 @@ for (let file of files) {
   let name = file.replace('.js', '')
   apps[name] = (await import(`./app/${file}`))[name]
 }
+
+setTimeout(async function () {
+  // 群名片复位
+  Bot.gl.forEach((v, k) => {
+    Bot.pickGroup(k).setCard(Bot.uin, Bot.nickname);
+  });
+}, 1000)
+
+
 let index = { auto: {} }
 export const auto = index.auto || {}
 export { apps }
