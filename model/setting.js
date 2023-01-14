7bd53f98e9cba9c95c9a50ca109494fc9e6467ee
Nwflower <1679659@qq.com>
2023-01-14 12:26:47 +0800

    新增了一些代码注释

--- model/setting.js
+++ model/setting.js
@@ -20,26 +20,30 @@ class Setting {
     this.watcher = { config: {}, def: {} }
   }
 
+  // 配置对象化 用于锅巴插件界面填充
   merge () {
     let sets = {}
     let appsConfig = fs.readdirSync(this.defPath).filter(file => file.endsWith(".yaml"));
     for (let appConfig of appsConfig) {
+      // 依次将每个文本填入键
       let filename = appConfig.replace(/.yaml/g, '').trim()
       sets[filename] = this.getConfig(filename)
     }
     return sets
   }
 
+  // 配置对象分析 用于锅巴插件界面设置
   analysis(config) {
     for (let key of Object.keys(config)){
       this.setConfig(key, config[key])
     }
   }
 
+  // 获取对应模块数据文件
   getData (path, filename) {
     path = `${this.dataPath}${path}/`
     try {
-      if (!fs.existsSync(`${path}${filename}.yaml`)){ return }
+      if (!fs.existsSync(`${path}${filename}.yaml`)){ return false}
       return YAML.parse(fs.readFileSync(`${path}${filename}.yaml`, 'utf8'))
     } catch (error) {
       logger.error(`[${filename}] 读取失败 ${error}`)
@@ -47,10 +51,12 @@ class Setting {
     }
   }
 
+  // 写入对应模块数据文件
   setData (path, filename, data) {
     path = `${this.dataPath}${path}/`
     try {
       if (!fs.existsSync(path)){
+        // 递归创建目录
         fs.mkdirSync(path, { recursive: true });
       }
       fs.writeFileSync(`${path}${filename}.yaml`, YAML.stringify(data),'utf8')
@@ -60,18 +66,22 @@ class Setting {
     }
   }
 
+  // 获取对应模块默认配置
   getdefSet (app) {
     return this.getYaml(app, 'def')
   }
 
+  // 获取对应模块用户配置
   getConfig (app) {
     return { ...this.getdefSet(app), ...this.getYaml(app, 'config') }
   }
 
+  // 设置对应模块用户配置
   setConfig (app, Object) {
     return this.setYaml(app, 'config', { ...this.getdefSet(app), ...Object})
   }
 
+  // 将对象写入YAML文件
   setYaml (app, type, Object){
     let file = this.getFilePath(app, type)
     try {
@@ -82,6 +92,7 @@ class Setting {
     }
   }
 
+  // 读取YAML文件 返回对象
   getYaml (app, type) {
     let file = this.getFilePath(app, type)
     if (this[type][app]) return this[type][app]
@@ -96,6 +107,7 @@ class Setting {
     return this[type][app]
   }
 
+  // 获取YAML文件目录
   getFilePath (app, type) {
     if (type === 'def') return `${this.defPath}${app}.yaml`
     else {
@@ -111,6 +123,7 @@ class Setting {
   }
 
 
+  // 监听配置文件
   watch (file, app, type = 'def') {
     if (this.watcher[type][app]) return
 
@@ -124,7 +137,6 @@ class Setting {
     })
     this.watcher[type][app] = watcher
   }
-
 }
 
 export default new Setting()
