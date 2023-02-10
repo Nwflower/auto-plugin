49b3a05f2ad4883ee308d6c368805a7eb612f401
Nwflower <1679659@qq.com>
2023-02-10 21:05:00 +0800

    修复api失效

--- model/autoGroupName/百度热搜.js
+++ model/autoGroupName/百度热搜.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch'
 export async function NameCardContent() {
-  let res = await fetch('https://tenapi.cn/baiduhot/').catch((err) => logger.error(err))
+  let res = await fetch('https://tenapi.cn/v2/baiduhot/').catch((err) => logger.error(err))
   if (!res) { return false }
   res = await res.json()
-  let result = res.list[0].name
+  let result = res.data[0].name
   if (result.length<=8) result = '百度热搜:' + result
   return result
 }
