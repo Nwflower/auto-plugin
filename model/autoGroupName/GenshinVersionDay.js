2d24d960e178626738f94c9106f794fb7753f101
西北一枝花 <1679659@qq.com>
2023-04-02 10:51:26 +0800

    更新群名片的一些API

--- model/autoGroupName/GenshinVersionDay.js
+++ model/autoGroupName/GenshinVersionDay.js
@@ -4,28 +4,35 @@
  * 禁止以任何形式 二次开源 倒卖 等
  */
 export function NameCardContent() {
-  // 基准版本
-  let Version = 3.5
-  let baseTime = new Date('2023-3-1 11:00:00').getTime()
-
-  let nowTime = new Date().getTime()
+  // 原神版本号
+  let Versions = [36, 37, 38, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50]
+  let Index = 0
 
   // 获取持续时间
+  let baseTime = new Date('2023-4-12 11:00:00').getTime()
+  let nowTime = new Date().getTime()
   let duringTime = baseTime - nowTime
+
+  // 推算版本
   while (duringTime <= 0){
-    // 版本+0.1 同时时间+42天
     duringTime += 42 * 24 * 60 * 60 * 1000
-    Version += 0.1
+    Index += 1
   }
+
+  // 计算版本号并取到小数点后一位
+  let Version = (Number(Versions[Index])/10).toFixed(1)
 
   // 获取天数并取整
   let days = Math.floor(duringTime / (24 * 3600 * 1000))
   let leave1 = duringTime % (24 * 3600 * 1000);
+
   // 获取小时数并取整
   let hours = Math.floor(leave1 / (3600 * 1000));
   let leave2 = leave1 % (3600 * 1000);
+
   // 获取分钟数并取整
   let minutes = Math.floor(leave2 / (60 * 1000));
 
-  return `离原神${Version.toFixed(1)}还有${days}天${hours}小时${minutes}分钟`
+  // 字符串处理
+  return `离原神${Version}还有${days}天${hours}小时${minutes}分钟`
 }
