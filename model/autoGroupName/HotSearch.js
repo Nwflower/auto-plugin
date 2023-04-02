fdf5a7c8008d0ce2e6f8298d639fdef740fa377d
西北一枝花 <1679659@qq.com>
2023-04-02 14:15:41 +0800

    修复bug

--- model/autoGroupName/HotSearch.js
+++ model/autoGroupName/HotSearch.js
@@ -7,7 +7,7 @@ export async function NameCardContent() {
   let res = await fetch('https://tenapi.cn/v2/weibohot').catch((err) => logger.error(err))
   if (!res) { return false }
   res = await res.json()
-  let result = res[0].data
+  let result = res.data
   let hot1 = result[0].name
   if (hot1.length<=8) hot1 = '微博热搜:' + hot1
   return hot1
